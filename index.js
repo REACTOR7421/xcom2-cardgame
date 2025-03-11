39455a2a580e248ffda92408d9f6675625a8fca9
REACTOR7421 <szep.roland2011@gmail.com>
2025-03-11 20:37:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000; // Render által adott port vagy 3000-es fallback
 
+app.use(express.static("public")); // Ha van egy 'public' mappa a statikus fájloknak
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
