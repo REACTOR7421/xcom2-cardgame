2a51c62fb09ffa648c36eb1979502f4008ad6415
REACTOR7421 <szep.roland2011@gmail.com>
2025-03-11 20:07:14 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XCOM 2 Kártyajáték</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="#">Főoldal</a></li>
+            <li><a href="#">Összes kártya</a></li>
+            <li><a href="#">Tutorial</a></li>
+            <li><a href="#">Játék</a></li>
+        </ul>
+    </nav>
+    <header>
+        <h1>XCOM 2 Kártyajáték</h1>
+        <p>Üdvözöllek a kártyajáték hivatalos oldalán! Hamarosan több információ érkezik.</p>
+    </header>
+</body>
+</html>
