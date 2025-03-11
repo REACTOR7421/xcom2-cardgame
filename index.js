fd106543bbdf1336573a2e26d40bdf917d7faf19
REACTOR7421 <szep.roland2011@gmail.com>
2025-03-11 19:44:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-// Statikus fájlok kiszolgálása (pl. HTML, CSS, JS)
-app.use(express.static("public"));
+// Statikus fájlok kiszolgálása
+app.use(express.static('public'));
 
-// Alap útvonal
-app.get("/", (req, res) => {
-  res.send("XCOM 2 Kártyajáték - Hamarosan!");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-// Szerver indítása
 app.listen(PORT, () => {
-  console.log(`Szerver fut: http://localhost:${PORT}`);
+    console.log(`Szerver fut: http://localhost:${PORT}`);
 });
