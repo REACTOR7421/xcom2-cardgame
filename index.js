400dbf0da53c56cd5459989f02a1b5245a391bcb
REACTOR7421 <szep.roland2011@gmail.com>
2025-03-11 20:11:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 10000;
 
-// Statikus fájlok kiszolgálása
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-app.listen(PORT, () => {
-    console.log(`Szerver fut: http://localhost:${PORT}`);
-});
