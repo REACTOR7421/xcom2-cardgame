f2cc22742e9a1184ca779a530811a41dc3accdec
REACTOR7421 <szep.roland2011@gmail.com>
2025-03-13 19:40:17 +0100

    Update cards.js

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,1 +1,14 @@
 
+// Az összes kártya alapneve (ritkaságok nélkül)
+const cards = [
+    "katona", "tiszt", "pajzshordo", "pap", "tisztito", "arctalan", "muton", "tombolo",
+    "arkon", "avatar", "babozo", "bunteto", "cyberdisc", "lebego", "fejlettlebego",
+    "kapuorzo", "kereso", "mec", "mechtoid", "outsider", "szektoid", "tabornok",
+    "thinman", "turret", "vipera",
+    
+    // Speciális kártyák, amelyeknek nincs ritkasági verziójuk
+    "vipera_king", "tombolo_king", "arkon_king", "warlock",
+    "assasin", "hunter", "elders", "uber_elders"
+];
+
+// Ezt a fájlt be kell tölteni a HTML-ben, hogy a loadCards.js használhassa!
